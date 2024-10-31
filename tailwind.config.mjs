45958f7bf50c0335f1955ac107b77f93fd7f993e
hwncito <juanchosperoni@hotmail.com>
2024-10-30 22:19:01 -0300

    coaching section and cards finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-import animations from '@midudev/tailwind-animations'
+import animations from '@midudev/tailwind-animations';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: { colors: { 'primary-500': '#FF885B', 'primary-600': '#F6784F' } },
+    extend: {
+      colors: {
+        'primary-500': '#FF885B',
+        'primary-600': '#F6784F',
+        'secondary-500': '#557C56',
+      },
+      rotate: {
+        135: '135deg',
+      },
+    },
   },
   plugins: [animations],
 };
